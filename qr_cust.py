e0e4850b3f4811af2c19a3df4f73550400237984
Rohan Tyagi <tyagirohan798@gmail.com>
2024-08-01 12:18:37 +0530

    Final commit of the project

---
+++ qr_cust.py
@@ -0,0 +1,12 @@
+# The given below code helps to customize the qrcode according to the user requirement.
+
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,
+                    error_correction = qrcode.constants.ERROR_CORRECT_H,
+                    box_size=20, border=4,)
+qr.add_data("https://www.linkedin.com/in/rohan-tyagi-245155240/")
+qr.make(fit=True)
+img = qr.make_image(fill_color="red", back_color = "blue")
+img.save("Linkedin_cust.png")
